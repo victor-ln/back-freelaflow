3dbb750474c60323f35517f9d723f0e3545105ff
Laylon Gilson dos Santos Gionda <57443860+LaylonGit@users.noreply.github.com>
2025-02-17 14:15:07 -0300

    primeiro commit

---
+++ src/main/java/com/freelaflow/back_freelaflow/BackFreelaflowApplication.java
@@ -0,0 +1,13 @@
+package com.freelaflow.back_freelaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFreelaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFreelaflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelaflow/back_freelaflow/BackFreelaflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelaflow.back_freelaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFreelaflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
